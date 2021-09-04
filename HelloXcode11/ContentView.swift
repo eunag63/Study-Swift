3839d4113794cbf75b7ef9466aece425b4e79fd4
eunag63 <77442567+eunag63@users.noreply.github.com>
2021-09-05 03:25:37 +0900

    Initial Commit

---
+++ HelloXcode11/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloXcode11
+//
+//  Created by 김은아 on 2021/09/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
