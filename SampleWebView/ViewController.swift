166ba9dd7669664f2d0f9bcaf4ef4606b8d3a346
eunag63 <77442567+eunag63@users.noreply.github.com>
2021-09-05 04:09:40 +0900

    웹뷰, 옵셔널 바인딩

--- SampleWebView/ViewController.swift
+++ SampleWebView/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //url string을 준비한다
+        let urlString = "https://www.google.com"
+        //url을 request로 만들기
+        
+        if let url = URL(string: urlString) { //unwrap - 옵셔널 바인딩
+            let urlReq = URLRequest(url: url)
+            WebViewMain.load(urlReq)
+        }
     }
-
-
+    
+    @IBOutlet weak var WebViewMain: WKWebView!
+    
+    
 }
 
