0364411cdc21841c5b50f0d78d5994211fec0f0f
eunag63 <77442567+eunag63@users.noreply.github.com>
2021-09-07 17:12:03 +0900

    뉴스앱 만들기 & 테이블뷰 3 - 뉴스 제목 보여주기

--- SentiTable/ViewController.swift
+++ SentiTable/ViewController.swift
@@ -8,9 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    //1. http 통신 방법 - urlsession
+    //2. JSON 데이터 형태 {"돈":"100"} {["100","500","1000"]}
+    //3. 테이블뷰의 데이터 매칭!! + 통보, 그리기
+    var newsData :Array<Dictionary<String, Any>>?
+    
+    
+    func getNews(){
+        let task = URLSession.shared.dataTask(with: URL(string: "https://newsapi.org/v2/top-headlines?country=kr&apiKey=16d3bddc346a4f638ee1f70ef47a09eb")!) { (data, response, error) in
+            //어떠한 정보를 http 통신을 하면 아래 코드로 json 데이터가 들어옴
+            if let dataJson = data {
+                //json으로 변환
+                do {
+                    let json = try JSONSerialization.jsonObject(with: dataJson, options: []) as! Dictionary<String, Any>
+                    
+                    //dictionary
+                    let articles = json["articles"] as! Array<Dictionary<String, Any>>
+                    self.newsData = articles
+                    
+                    //그리기
+                    DispatchQueue.main.async {
+                        self.TableViewMain.reloadData()
+                    }
+                    
+                    //반복문으로 타이틀 다 가져오기
+//                    for (idx, value) in articles.enumerated() {
+//                        if let v = value as? Dictionary<String, Any> {
+//                            print("\(v["title"])")
+//                            v["description"]
+//                        }
+//
+//                    }
+                }
+                catch{}
+                
+            }
+        }
+        
+        task.resume()
+    }
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //데이터 몇개 숫자
-        return 100
+        
+        //뉴스의 숫자로 바꾸기
+        if let news = newsData {
+            return news.count
+        }
+        else{
+            return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -21,10 +70,24 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         //2.스토리보드 + id : 실전
         let cell = TableViewMain.dequeueReusableCell(withIdentifier: "Type1", for: indexPath) as! Type1
-        cell.LabelText.text = "\(indexPath.row)"
+        
+        let idx = indexPath.row
+        if let news = newsData{
+            
+            let row = news[idx]
+            if let r = row as? Dictionary<String, Any> {
+                if let title = r["title"] as? String{
+                    cell.LabelText.text = title
+                }
+            }
+        }
+        
+        
         
         return cell
     }
+    
+   
     
     //3. 클릭시
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -32,15 +95,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //함수 호출
         
         TableViewMain.delegate = self
         TableViewMain.dataSource = self
         
+        getNews()
+        
     }
+    
     
     //tableview 테이블로 된 뷰 = 여러개이 행이 모여있는 목록 뷰(화면)
     //깔끔하게 보여주기 위해서 사용
